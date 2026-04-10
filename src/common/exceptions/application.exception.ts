8e9688d2f2b170f4654a2f9d04e5ff909b2d09e6
hazzem-web <hazzem.eljoker@gmail.com>
2026-04-10 12:00:00 +0200

    feat(exceptions): handle common HTTP exceptions extending ApplicationError

--- src/common/exceptions/application.exception.ts
+++ src/common/exceptions/application.exception.ts
@@ -14,3 +14,44 @@ export class ApplicationError extends Error implements AError {
 
 
 
+
+export class BadRequestException extends ApplicationError {
+    constructor(message:string , cause?:unknown){
+        super(message , 400 , { cause })
+    }
+};
+
+
+export class conflictException extends ApplicationError { 
+    constructor(message:string , cause?:unknown){
+        super(message , 409 , { cause })
+    }
+};
+
+
+export class NotFoundException extends ApplicationError { 
+    constructor(message:string , cause?:unknown){
+        super(message , 404 , { cause })
+    }
+};
+
+
+
+export class UnAuthorizedException extends ApplicationError { 
+    constructor(message:string , cause?:unknown){
+        super(message , 401 , { cause })
+    }
+};
+
+
+
+export class ForbiddenException extends ApplicationError { 
+    constructor(message:string , cause?:unknown){
+        super(message , 403 , { cause })
+    }
+};
+
+
+
+
+

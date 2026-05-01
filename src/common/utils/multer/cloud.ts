4d7b15faaf84e7f23e878755344ca92b426f8866
hazzem-web <hazzem.eljoker@gmail.com>
2026-05-01 12:00:00 +0300

    fix(cloud) implement diskstorage in multer

--- src/common/utils/multer/cloud.ts
+++ src/common/utils/multer/cloud.ts
@@ -1,6 +1,16 @@
 import multer from "multer"
-
+import {tmpdir} from "node:os";
+console.log(tmpdir());
 export const uploadFile = ()=> {
-    const storage = multer.memoryStorage();  
+    // const storage = multer.memoryStorage();  
+
+    const storage = multer.diskStorage({
+        destination: (req,file,cb){
+            cb(null, tmpdir());
+        } , 
+        filename: (req,file,cb){
+            const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9) + "-" + file.originalname;
+        }
+    })
     return multer({storage});   
 }

c980ea2ab0793dd003b41d04b5a74e5e63056674
hazzem-web <hazzem.eljoker@gmail.com>
2026-05-01 12:00:00 +0300

    fix(cloud) implement dynamic changing between storage types in multer cloud

--- src/common/utils/multer/cloud.ts
+++ src/common/utils/multer/cloud.ts
@@ -1,15 +1,22 @@
 import multer from "multer"
 import {tmpdir} from "node:os";
+import { MulterEnum } from "../../enums/multer.enum";
+import { file } from "zod";
 console.log(tmpdir());
-export const uploadFile = ()=> {
+export const uploadFile = ({
+    storageKey = MulterEnum.memoryStorage
+}: { 
+    storageKey?:MulterEnum
+})=> {
     // const storage = multer.memoryStorage();  
 
-    const storage = multer.diskStorage({
-        destination: (req,file,cb){
+    const storage = storageKey == MulterEnum.memoryStorage ? multer.memoryStorage(): multer.diskStorage({
+        destination(req,file,cb){
             cb(null, tmpdir());
-        } , 
-        filename: (req,file,cb){
-            const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9) + "-" + file.originalname;
+        },
+        filename(req,file,cb){
+            const uniqueSuffix =( Date.now() + "-" + Math.round(Math.random() * 1e9 )+ "-" + file.originalname);
+            cb(null, `${file.fieldname}-${uniqueSuffix}`);
         }
     })
     return multer({storage});   
